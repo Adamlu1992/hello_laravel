b3830c8b0c5c97b3710e76adca8e33047ec0ad06
Adam <adamly1992@163.com>
2020-01-19 02:18:04 +0000

    用户显示界面

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,12 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/signup', 'UsersController@create')->name('signup');//注册
+Route::resource('users','UsersController');
+//上面一行代码将等同于：
+// Route::get('/users', 'UsersController@index')->name('users.index');//显示所有用户列表
+// Route::get('/users/create', 'UsersController@create')->name('users.create');//创建用户的页面
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');//显示用户个人信息
+// Route::post('/users', 'UsersController@store')->name('users.store');//创建用户
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');//编辑用户个人资料
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');//更新用户
+// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');//删除用户
